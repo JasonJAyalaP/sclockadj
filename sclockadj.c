e9bf84e3a400f7a8ef01e5f00dcefc013d0a9efe
JasonJAyalaP <JasonJAyalaP+Github@Gmail.com>
2017-06-02 20:22:28 -0500

    cleanup, better error handling

--- sclockadj.c
+++ sclockadj.c
@@ -3,17 +3,16 @@
 #include <sys/time.h>
 #include <stdlib.h>
 
-int change_time_by_nanoseconds(long long add_ns)
+/* receive time adjustment, negative or positive, in nanoseconds */
+/* long long 64bit is at least −9,223,372,036,854,775,807 to +9,223,372,036,854,775,807 */
+/* exits program upon failure */
+void change_time_by_nanoseconds(long long add_ns)
 {
-    /* receive time adjustment, negative or positive, in nanoseconds
-       long long on 64bit is at least a range of
-       −9,223,372,036,854,775,807 to +9,223,372,036,854,775,807 */
-    struct timespec tps; // tv_sec; tv_nsec
-    /* get current time in seconds since epoch + nanoseconds offset */
-    if (clock_gettime(0, &tps) == -1)
+    struct timespec tps; /* tv_sec; tv_nsec */
+    if (clock_gettime(0, &tps) == -1) /* get current time in seconds since epoch + nanoseconds offset */
     {
-        perror("getclock");
-        return EXIT_FAILURE;
+        perror("Failed to get current time!");
+        exit(EXIT_FAILURE);
     }
     /* combine seconds and nanoseconds offset in order to add nanoseconds*/
     long long ns_since_epoch =
@@ -29,20 +28,19 @@ int change_time_by_nanoseconds(long long add_ns)
     /* set time with new values */
     if (clock_settime(0, &tps) == -1)
     {
-        perror("setclock");
-        return EXIT_FAILURE;
+        perror("Failed to change clock!");
+        exit(EXIT_FAILURE);
     }
-    return EXIT_SUCCESS;
 }
 
+/* intended to be used only by sdwdate with sane inputs */
 int main(int argc, char *argv[])
 {
-    /* intended to be used only by Sdwdate with sane inputs */
     if (argc < 1) return 1;
     long long ns_time_change = atoll(argv[1]); /* convert argv string into long long */
     if (ns_time_change == 0) return 1; /* exit if atoll fails */
     /* since nanosecond jump is fixed, we can count the number of complete jumps. */
-    /* llabs handles negative numbers. */
+    /* llabs for negative numbers */
     long long number_of_full_jumps = llabs(ns_time_change) / 5000000;  /* times we'll move clock by 5,000,000 ns at a time */
     long long last_jump_nanoseconds = llabs(ns_time_change) % 5000000; /* then add remaining < 5,000,000 ns */
 
@@ -50,7 +48,7 @@ int main(int argc, char *argv[])
     {
         for (unsigned i = 0; i < number_of_full_jumps; ++i) 
         {
-            sleep(1);  /* a 1 second wait imitates ntpdate */
+            //sleep(1);  /* a 1 second wait imitates ntpdate */
             change_time_by_nanoseconds(5000000); /* 5,000,000 ns imitates ntpdate */
         }
         change_time_by_nanoseconds(last_jump_nanoseconds);
@@ -59,10 +57,10 @@ int main(int argc, char *argv[])
     {
         for (unsigned i = 0; i < number_of_full_jumps; ++i) 
         {
-            sleep(1);
+            //sleep(1);
             change_time_by_nanoseconds(-5000000);
         }
         change_time_by_nanoseconds(-last_jump_nanoseconds); /* negative of absolute value imitates Euclidean modulo */
     }
-    return 0;
+    return EXIT_SUCCESS;
 }
